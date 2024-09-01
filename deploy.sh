690078e13fd0ef48970a14b4adec97a3087d7b57
Shamil Magomedov <shomainkognito@yandex.ru>
2024-09-01 23:42:00 +0300

    #fix project deploy settings

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# если публикация происходит по адресу https://<USERNAME>.github.io
+# git push -f git@github.com:womajies/starpets.gg.github.io.git master
+
+# если публикация происходит по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:womajies/starpets.gg.git master:gh-pages
+
+cd -
